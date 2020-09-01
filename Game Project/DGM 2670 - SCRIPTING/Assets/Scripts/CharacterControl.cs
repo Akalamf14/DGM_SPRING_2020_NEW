7dc3c3a2f0364bb8f3341c90bd7b323d30a24322
Unknown <akalamf14@gmail.com>
2020-09-01 16:18:43 -0600

    CharacterControl - Movement and Jump

---
+++ Game Project/DGM 2670 - SCRIPTING/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterControl : MonoBehaviour
+{
+    private CharacterController controller;
+    public Vector3 movement;
+    public float gravity = 9.81f;
+    public float moveSpeed = 3f;
+    public float fastMoveSpeed;
+    public float jumpforce = 10f;
+    public int jumpCountMax;
+    public float rotateSpeed;
+    private Vector3 rotateMovement;
+
+
+    //PROBLEM SOLVE - HOW TO MAKE IT SO OBJECT DOESN'T MOVE 
+    //VERTICALLY OR HORIZONTALLY WHILE JUMPING!!!
+
+    //PROBLEM SOLVE RUN!
+
+    //PROBLEM SOLVE ROTATION
+
+    //PROBLEM SOLVE DOUBLE JUMP!! 
+
+    //PROBLEM SOLVE SUSPENSION IN AIR WHILE JUMPING?
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+       
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpforce;
+        }
+
+        if(controller.isGrounded)
+        {
+            movement.y = 0;
+        }
+        else
+        {
+             movement.y -= gravity;
+        }
+
+        movement.x = Input.GetAxis("Horizontal")* moveSpeed;
+        movement.z = Input.GetAxis("Vertical")* moveSpeed;
+
+        controller.Move(movement*Time.deltaTime);
+    }
+}
