1332a50d1cee48fd163a4b02b2490a3cf582e884
Unknown <akalamf14@gmail.com>
2020-09-12 23:30:51 -0600

    First attempt at solving health and spawn points

---
+++ Game Project/DGM 2670 - SCRIPTING/Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour
+{
+    public IntData startHealth, currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startHealth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
