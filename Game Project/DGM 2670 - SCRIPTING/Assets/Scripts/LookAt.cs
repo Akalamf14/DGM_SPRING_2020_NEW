0d456391bc69775d6003e444acb3cd8f32e7822d
Unknown <akalamf14@gmail.com>
2020-09-17 12:01:37 -0600

    Lerp and Look AT

---
+++ Game Project/DGM 2670 - SCRIPTING/Assets/Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform lookObj;
+
+
+    void Update()
+    {
+        transform.LookAt(lookObj);
+        var transformRotation = transform.eulerAngles;
+        transformRotation.x = 0;
+        transform.rotation = Quaternion.Euler(transformRotation);
+    }
+}
