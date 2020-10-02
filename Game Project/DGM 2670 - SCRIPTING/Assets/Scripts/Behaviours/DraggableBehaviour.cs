55062ffb4d497fa5a9bc473aa6358b128d0c3883
Unknown <akalamf14@gmail.com>
2020-10-02 09:49:54 -0600

    Working on Draggable Behaviour Script and Understanding Unity Events with Script

---
+++ Game Project/DGM 2670 - SCRIPTING/Assets/Scripts/Behaviours/DraggableBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class DraggableBehaviour : MonoBehaviour
+{
+    private Vector3 offsetPosition;
+    private Vector3 newPosition;
+    private Camera cam;
+
+    private bool canDrag {get; set;}
+    public UnityEvent onDrag, onUp;
+    public bool Draggable {get; set;}
+
+    private void Start()
+    {
+        cam = Camera.main;
+        Draggable = true;
+    }
+
+    public IEnumerator OnMouseDown()
+    {
+        onDrag.Invoke();
+        offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+        yield return new WaitForFixedUpdate();
+        canDrag = true;
+
+        while(canDrag)
+        {
+            yield return new WaitForFixedUpdate();
+            newPosition = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
+            transform.position = newPosition;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+       canDrag = false;
+       if(Draggable)
+       {
+           onUp.Invoke();
+       }
+    }
+
+}
