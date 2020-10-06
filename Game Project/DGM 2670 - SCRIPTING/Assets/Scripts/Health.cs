f604259e9ee593f8fa6e6f3a0cbf5f1b77a4d341
Unknown <akalamf14@gmail.com>
2020-10-05 22:29:39 -0600

    Made changes to health script

---
+++ Game Project/DGM 2670 - SCRIPTING/Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public IntData startHealth, currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth.value = startHealth.value;
+        
+    }
+
+    private IEnumerator OnCollisionEnter(Collider other)
+    {
+        GetComponent<Collider>();
+        currentHealth.value -= 1;
+        print(currentHealth.value);
+    }
+    
+
+
+}
