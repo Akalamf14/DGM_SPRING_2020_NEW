c8141d358101b88910b63f9a24f422aaf195b544
Unknown <akalamf14@gmail.com>
2020-10-07 17:18:20 -0600

    AI Script with navmesh

---
+++ Game Project/DGM 2670 - SCRIPTING/Assets/Scripts/Behaviours/AIBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    private bool canNavigate = true;
+    private WaitForFixedUpdate wffu;
+    public float holdTime = 1f;
+    private WaitForSeconds wfs;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        wfs = new WaitForSeconds(holdTime);
+    }
+
+    private IEnumerator Navigate()
+    {
+        canNavigate = true;
+        yield return wfs;
+        while(canNavigate)
+        {
+            yield return wfs;
+            agent.destination = player.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collision other)
+    {
+        canNavigate = false;
+        StartCoroutine(Navigate());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canNavigate = false;
+    }
+}
