d75630b10e32d172d1065a3f0753ec5bd6b3b8e4
Unknown <akalamf14@gmail.com>
2020-10-10 20:48:09 -0600

    Exploring Scripts related to AI

---
+++ Game Project/DGM 2670 - SCRIPTING/Assets/Scripts/AIScripts/AIHunt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "Hunt", menuName = "Ai/Function/Hunt")]
+public class AIHunt : AIBase
+{
+    public GameAction destinationAction;
+    [HideInInspector] public Transform destination;
+
+    protected virtual void OnEnable()
+    {
+        if(destinationAction != null)
+        {
+            destinationAction.raise += OnRaise;
+        }
+    }
+
+    private void OnRaise(object obj)
+    {
+        destination = obj as Transform;
+    }
+
+    public override void RunAgent(NavMeshAgent agent)
+    {
+        agent.destination = (destination != null ? destination.position : agent.transform.position);
+
+    }
+
+}
