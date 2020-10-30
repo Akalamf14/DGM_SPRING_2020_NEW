7afc82e03ab375c3550143e6345f3d9bfd55dc45
Unknown <akalamf14@gmail.com>
2020-10-30 10:51:54 -0600

    Attach on Trigger

---
+++ Game Project/DGM 2670 - SCRIPTING/Assets/Scripts/Behaviours/HideOnPlayBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class HideOnPlayBehaviour : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+}
